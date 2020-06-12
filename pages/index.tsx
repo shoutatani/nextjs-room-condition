5160032b14d439dfb94d3a57d44060e573c0dd7a
Shota Taniguchi <10176164+shoutatani@users.noreply.github.com>
2020-06-13 00:32:17 +0900

    Setup OGP

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,18 @@ export default function Home() {
   return (
     <Container>
       <Head>
-        <title>shoutatani's Room Condition Data</title>
+        <title>My room's air condition Data</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="僕のお部屋のCO2濃度, 大気圧, 湿度, 室温を表示する僕得アプリです。" />
+        <meta name="og:description" content="僕のお部屋のCO2濃度, 大気圧, 湿度, 室温を表示する僕得アプリです。" />
+        <meta name="og:title" content="My room's air condition data" />
+        <meta name="og:site_name" content="My room's air condition data" />
+        <meta name="og:type" content="website" />
+        <meta name="og:image" content="https://my-room-condition.tan-shio.com/eye-catch-my-rooms-air-condition-data.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="shoutatani" />
+        <meta name="twitter:title" content="My room's air condition data" />
+        <meta name="twitter:description" content="僕のお部屋のCO2濃度, 大気圧, 湿度, 室温を表示する僕得アプリです。" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
