bf53133738b5c5f8eebba742e5da0cb87f740a25
shoutatani <10176164+shoutatani@users.noreply.github.com>
2023-05-09 13:32:22 +0900

    introduced @vercel/analytics/react

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import { AppProps } from "next/app";
 import { Abel } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const abel = Abel({ weight: "400", subsets: ["latin"], display: "swap" });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main className={abel.className}>
-      <Component {...pageProps} />;
-    </main>
+    <>
+      <main className={abel.className}>
+        <Component {...pageProps} />;
+      </main>
+      <Analytics />
+    </>
   );
 }
 
